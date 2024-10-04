20de81c2a21b98973393fe80ec9a09d63e311fcc
afirthes <afirthes@gmail.com>
2024-10-04 19:57:52 +0300

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Icetusk on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
